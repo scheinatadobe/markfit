d1dfe63994f0aa5b925299f4a787606f90f943e2
Andrew Schein <aschein@aschein-MacBookPro1.(none)>
2013-01-23 19:50:24 -0800

    backing up during packaging work.

--- linear_model/lm_model.py
+++ linear_model/lm_model.py
@@ -4,11 +4,11 @@
 import numpy as np
 import patsy
 import pandas
-#from sweep_based_solver import solve
-
-from adjust_based_solver import solve 
-
+#import sweep_based_solver
+import adjust_based_solver
+import qr_based_solver
 import scipy.stats as stats
+import scipy.linalg as linalg
 from sys import stdout, stderr
 
 #fitting strategies coded as variables:
@@ -80,7 +80,7 @@ class LinearModelSummary :
         lines.append("")
         print >>stdout, "\n".join(lines)     
 
-def computeSummary(A,n,p,SSTO,column_names,formula) :
+def computeSummary(beta,betaSigmaSq,SSE,SSTO,n,p,column_names,formula) :
     """
     Summaryize the linear regression model
     #y    - the dependent variable
@@ -96,45 +96,24 @@ def computeSummary(A,n,p,SSTO,column_names,formula) :
 
     """
     
-    XXn1       =   A[:p,:p]
-    XXn1        =   A[:p,p+1:]
-    #print "diag: ", np.diag(XXn1)
-    beta       =   A[:p,p]
     assert n>p
     assert p>1
-    #yHat       =   model.predict(X)
-    #if len(yHat.shape) > 1 and yHat.shape[1] == 1 : yHat = yHat.reshape(yHat.shape[0])
-    #residue    =   y - yHat
-    #ones       =   np.ones([y.shape[0],y.shape[0]])
-    #SSE         =   np.dot(residue,residue)
-    SSE        = A[p,p]
-    #print "SSE: ", SSE
     MSE        =   SSE / (n - p)
-    #print "MSE: ", MSE, np.diag(XXn1*MSE)
-    #yBar       =   np.mean(y)
-    #residue    =   yBar - yHat
-    #SSR        =   np.dot(residue,residue)
     SSR        =   SSTO - SSE #fundamental property of linear regression     
-    MSR        =   SSR/(p-1)
+    MSR        =   SSR/(p - 1)
     f_stat     =   MSR/MSE #TODO: sensible checking
     pVal       =   1 - stats.f.cdf(f_stat,p-1,n-p)
     RSq        =   SSR / SSTO
     #On to estimation of parameter-specific values
     #for now hard code single response
-    #print XXn1.shape, np.diag(XXn1)
-    S  = np.sqrt(np.diag(XXn1 * MSE)) #estimate parameter sigma
-    
-    #print "S:", S
+    S  = np.sqrt(betaSigmaSq * MSE) #estimate parameter sigma
     paramT     = beta / S
     paramT[S == 0] = 0
-    #self.SE    = self.S[0] / math.sqrt(self.n-self.p)
     paramPVal  = 2 * (1 - stats.t.cdf(np.abs(paramT), n - p))
-    #print "shape for S: ", self.S.shape
-    #print "shape for t : ", self.t.shape
     return LinearModelSummary(n,p,beta,SSTO,SSE,MSE,SSR,MSR,f_stat,pVal,RSq,paramT,paramPVal,S,
                                 column_names,formula)
 
-def lm(formula,dataFrame,fitStrategy=Adjust) :
+def lm(formula,dataFrame,fitStrategy=Adjust,tolerance=1e-6) :
 
     """ 
     formula   - a patsy formula
@@ -143,52 +122,53 @@ def lm(formula,dataFrame,fitStrategy=Adjust) :
 
     We make summary computation optional because it adds overhead
     """
-    y,X = patsy.dmatrices(formula,dataFrame)    
-    #print X.design_info.term_names    
-    #print y.shape
+    y,X = patsy.dmatrices(formula,dataFrame)
     assert y.shape[1] == 1, "Multiple dependent variables are not yet supported."
-    
-    if fitStrategy == QR :
-	raise "Not implemented yet!"	
-    elif fitStrategy == "Sweep" :
-	raise "Not implemented yet!"
-	
     y = y.flatten()
     n   = y.shape[0]
     p   = X.shape[1]
-    
-    XX  = np.dot(X.T,X)
-    Xy  = np.array([np.dot(X.T,y)])
-    #print "p: ", p
-    #print XX.shape, Xy.shape
-    #build augmented matrix "A" to perform elimination on
-    A = np.append(XX,Xy.T,axis=1)
-    #print "Ashape: ", A.shape
-    A = np.append(A,np.identity(XX.shape[0]),axis=1)
-
-    #print "A first row shape: ", A.shape
-    yy = np.array([[np.dot(y,y)]])
-    #print Xy.shape, yy.shape
-    bottom = np.append(Xy,yy.T,1)
-    bottom = np.append(bottom,np.zeros([1,p]),1)
-    #print "A.shape: ", A.shape, "bottom.shape: ", bottom.shape
-    A = np.append(A,bottom,0)
-    #print "A diag: ", np.diag(A)
-    solve(A) 
-    #print "solved:", A.shape
-    #print A[:p,:p]
-    #print A[:p,p+1:]
      
+    XX  = np.dot(X.T,X)
+    #print X.design_info.term_names    
+    #print y.shape
+    
+    if fitStrategy == QR :
+        beta,betaSigmaSq,SSE = qr_based_solver.solve(X,y,tolerance=tolerance)
+    elif fitStrategy == "Adjust" or fitStrategy == "Sweep" :
+    	Xy  = np.array([np.dot(X.T,y)])
+    	A = np.append(XX,Xy.T,axis=1)
+    	if fitStrategy == "Adjust" : 
+		    A = np.append(A,np.identity(XX.shape[0]),axis=1)
+    	yy = np.array([[np.dot(y,y)]])
+    	bottom = np.append(Xy,yy.T,1)
+    	if fitStrategy == "Adjust" : 
+		    bottom = np.append(bottom,np.zeros([1,p]),1)
+    	A = np.append(A,bottom,0)
+        if fitStrategy == "Adjust" : 
+            adjust_based_solver.solve(A)
+            XXn1 = A[:p,p+1:]
+        else :
+            sweep_based_solver.solve(A)
+            XXn1 = A[:p,:p]
+        betaSigmaSq = np.diag(XXn1)
+        beta = A[:p,p]
+        SSE  = A[p,p]
+    else : 
+        raise "Invalid fit strategy selected: '" + fitStrategy + "'"
+    
     #The problem is solved.  On to computing ANOVA table and other diagnostics! 
     residWithMean = y - np.mean(y)
     SSTO = np.dot(residWithMean,residWithMean)
-    summary = computeSummary(A,n,p,SSTO,X.design_info.column_names,formula)
+    summary = computeSummary(beta,betaSigmaSq,SSE,SSTO,n,p,X.design_info.column_names,formula)
     #print summary.beta
     return LinearModelFit(summary.beta,summary)
 
 if __name__ == "__main__" :
     data = pandas.io.parsers.read_csv("salary2.txt")
-    fit = lm("sl ~ 1+sx+rk+yr+dg+yd",data)
+    fit = lm("sl ~ 1+sx+rk+yr+dg+yd+yd2",data,fitStrategy=QR)
+    fit.summary.write()
+    fit = lm("sl ~ 1+sx+rk+yr+dg+yd+yd2",data)
+
     #fit = lm("sl ~  dg + yd +yd2", data)
 
     fit.summary.write()
